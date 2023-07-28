2ffd95e15fd8a0ab12ea9802c7af8b9e8ae46c7c
Vladimir Likhachiov <likhachiovvl@gmail.com>
2023-07-28 23:08:31 +0300

    initial_commit

---
+++ classes.py
@@ -0,0 +1,183 @@
+import datetime as DT
+from collections import UserDict
+import os
+import pickle
+
+
+class AddressBook(UserDict):
+    def addRecord(self, record):
+        if record.name.value not in self.keys():
+            self.data[record.name.value] = record
+        else:
+            print("Name already exists. Please try to add phone command to add extra phone.")
+
+    def iterator(self, n, page=1):
+        count = 0
+        start = (page - 1) * n
+        end = page * n
+
+        for i, key in enumerate(self.keys()):
+            if i >= start and i < end:
+                yield key, self[key]
+                count += 1
+            elif i >= end:
+                break
+
+    def save_to_file(self, filename):
+        with open(filename, mode="wb") as file:
+            pickle.dump(self.data, file)
+            print("Address book save.")
+
+    def load_from_file(self, filename):
+        try:
+            with open(filename, 'rb') as f:
+                if os.stat(filename).st_size == 0:
+                    self.data = {}
+                else:
+                    self.data = pickle.load(f)
+                print("Address book loaded.")
+        except (FileNotFoundError, pickle.UnpicklingError):
+            with open(filename, 'wb') as f:
+                self.data = {}
+                pickle.dump(self.data, f)
+
+    def show_page(self, page_number=1, count=5):
+        num_pages = len(self) // count + (1 if len(self) % count > 0 else 0)
+        if page_number < 1 or page_number > num_pages:
+            return f"Invalid page number. Available pages: 1-{num_pages}"
+        out = f"|{' Page #':^10}{'|':^3}{'Contacts':^84}|\n"
+        out += '-' * 100 + '\n'
+        out += f"|{f' #{page_number}':^10}{'|':^3}{'Max Contacts per Page: ':<22}{count:^6}{'|':^3}\n"
+        out += '-' * 100 + '\n'
+        out += '| {:^20} | {:^40} | {:^20} |\n'.format(
+            'Name', 'Phones', 'Birthday Date')
+        out += '-' * 100 + '\n'
+        start = (page_number - 1) * count
+        end = page_number * count
+        if self.keys():
+            for i, (key, value) in enumerate(self.iterator(count, page=page_number), start=start):
+                out += value.print_record(i + 1)
+        else:
+            out += '| {:^100} |\n'.format('Address book is empty.')
+        out += '-' * 100 + '\n'
+        return out
+
+
+    def print_all(self):
+        out = '-'*100 + '\n'
+        out += '| {:^20} | {:^40} | {:^20} |\n'.format(
+            'Name', 'Phones', 'Birthday date')
+        out += '-'*100 + '\n'
+        if self.keys():
+            for key in self.keys():
+                out += self[key].print_record()
+        else:
+            out += '| {:^90} |\n'.format('Adress book is empty')
+        out += '-'*100 + '\n'
+        return out
+
+
+class Field:
+    def __init__(self):
+        self._value = None
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+
+class Name(Field):
+    pass
+
+
+class Birthday(Field):
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if value:
+            try:
+                self._value = DT.datetime.strptime(value, '%d-%m-%Y')
+            except ValueError:
+                print('Print date in format dd-mm-YYYY')
+                raise ValueError
+        else:
+            self._value = ''
+
+
+class Phone(Field):
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if not value.isdigit() and value:
+            print('Phone must be a number')
+            raise ValueError
+        self._value = value
+
+
+class Record:
+    def __init__(self, name, phone=None, birthday=None):
+        self.name = name
+        self.phones = []
+        if phone:
+            self.phones.append(phone)
+        self.birthday = birthday
+
+    def print_record(self):
+        if self.phones:
+            phones = ', '.join(phone.value for phone in self.phones)
+        else:
+            phones = 'No phones found.'
+        bd = str(self.birthday.value.date()
+                 ) if self.birthday.value else 'No birthday date.'
+        return ('| {:<20} | {:^40} | {:^20} |\n'.format(self.name.value, phones, bd))
+
+    def days_to_birthday(self):
+        if self.birthday.value:
+            bd = self.birthday.value
+            if DT.datetime(DT.datetime.now().year, bd.month, bd.day) > DT.datetime.now():
+                new_dt = DT.datetime(DT.datetime.now().year, bd.month, bd.day)
+            else:
+                new_dt = DT.datetime(
+                    DT.datetime.now().year + 1, bd.month, bd.day)
+            res = new_dt - DT.datetime.now()
+            return f"{res.days} days to {self.name.value} birthday!"
+        else:
+            return 'Have not found birthday date.'
+
+    def add_phone(self, phone: Phone):
+        if phone.value not in [phone.value for phone in self.phones]:
+            self.phones.append(phone)
+        else:
+            print("This phone already added.")
+
+    def add_birthday(self, birthday: Birthday):
+        if birthday.value != self.birthday.value and not self.birthday.value:
+            self.birthday = birthday
+        else:
+            print(f"{self.name.value} birthday already added.")
+
+    def del_phone(self, phone: Phone):
+        for n in self.phones:
+            if n.value == phone.value:
+                self.phones.remove(n)
+
+    def change_phone(self, old_phone: Phone, new_phone: Phone):
+        if old_phone.value == new_phone.value or new_phone.value in [phone.value for phone in self.phones]:
+            print("This phone alredy exists!")
+        elif old_phone.value not in [phone.value for phone in self.phones]:
+            print("This phone was not found!")
+        else:
+            for phone in self.phones:
+                if old_phone.value == phone.value:
+                    phone.value = new_phone.value
+            print("Phone changed.")
---
+++ main.py
@@ -0,0 +1,166 @@
+import classes
+
+
+address_book = classes.AddressBook()
+bot_working = True
+
+
+def input_error(func):
+    MAX_CONSECUTIVE_ERRORS = 5
+    def inner(*args, **kwargs):
+        consecutive_errors = 0
+        while bot_working:
+            try:
+                return func(*args, **kwargs)
+            except UnboundLocalError:
+                print('Enter command')
+            except TypeError:
+                print('Enter name and phone separated by a space!')
+            except KeyError:
+                print('This name was not found!')
+            except IndexError:
+                print('This name was found! Enter another name.')
+            except Exception as e:
+                print('Error:', e)
+            consecutive_errors += 1
+            if consecutive_errors >= MAX_CONSECUTIVE_ERRORS:
+                print("Exiting due to consecutive wrong commands.")
+                close()
+                break
+    return inner
+
+
+def start():
+    print("How may I help you today?")
+    address_book.load_from_file("address_book.bin")
+
+
+def show_page(page_number=1, count=5):
+    return address_book.show_page(page_number, count)
+
+
+def helper():
+    res = ''
+    for key in COMMANDS.keys():
+        res += f"{key}\n"
+    return "Available bot functions:\n" + res
+
+
+def close():
+    global bot_working
+    bot_working = False
+    address_book.save_to_file('address_book.bin')
+    return ("Good bye!\nBot stopped.")
+
+
+def hello():
+    return ('How can I help you?')
+
+
+@input_error
+def add_record(name, phone='', birthday=''):
+    rec_name = classes.Name()
+    rec_name.value = name
+
+    rec_phone = classes.Phone()
+    rec_phone.value = phone
+
+    rec_bd = classes.Birthday()
+    rec_bd.value = birthday
+
+    rec = classes.Record(rec_name, rec_phone, rec_bd)
+
+    address_book.addRecord(rec)
+    return rec.print_record()
+
+
+def change_phone(name, old_phone, new_phone):
+    rec = address_book[name]
+    o_ph = classes.Phone()
+    o_ph.value = old_phone
+    n_ph = classes.Phone()
+    n_ph.value = new_phone
+    if o_ph.value not in [phone.value for phone in rec.phones]:
+        print("This phone was not found!")
+    else:
+        rec.change_phone(o_ph, n_ph)
+        return rec.print_record()
+
+
+def add_phone(name, phone):
+    rec = address_book[name]
+    ph = classes.Phone()
+    ph.value = phone
+    rec.add_phone(ph)
+    return rec.print_record()
+
+
+def add_birthday(name, birthday):
+    rec = address_book[name]
+    bd = classes.Birthday()
+    bd.value = birthday
+    rec.add_birthday(bd)
+    return rec.print_record()
+
+
+def delete_phone(name, phone):
+    rec = address_book[name]
+    ph = classes.Phone()
+    ph.value = phone
+    rec.del_phone(ph)
+    return rec.print_record()
+
+
+def days_to_birthday(name):
+    rec = address_book[name]
+    return rec.days_to_birthday()
+
+
+def showall():
+    return address_book.print_all()
+
+
+def phone(name):
+    rec = address_book[name]
+    return rec.print_record()
+
+
+def unknown_command():
+    return "Unknown command, try again"
+
+
+def command_parse(user_input):
+    for key, cmd in COMMANDS.items():
+        if key in user_input.lower():
+            return cmd, user_input[len(key):].strip().split()
+    return unknown_command, []
+
+
+COMMANDS = {'hello': hello,
+            'days to bd': days_to_birthday,
+            'add phone': add_phone,
+            'add birthday': add_birthday,
+            'change phone': change_phone,
+            'delete phone': delete_phone,
+            'find phone': phone,
+            'show page': show_page,
+            'show all': showall,
+            'good bye': close,
+            'exit': close,
+            'close': close,
+            'add': add_record,
+            'help': helper,
+            }
+
+
+@input_error
+def main():
+    start()
+    while bot_working:
+        s = input()
+        command, arguments = command_parse(s)
+        print(command(*arguments))
+
+
+if __name__ == '__main__':
+    main()
